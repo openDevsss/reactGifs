c5c2b4e63e3f220ed46103507e9e48ed9ed438da
Sasha <sasha.shvets.2017@mail.ru>
2023-11-06 14:14:31 +0200

    feat: create page not found

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  TitleNotFound,
+  BoxDescriptionNotFound,
+  WrapperNotFound,
+  SubtitleNotFound,
+  DescriptionNotFound,
+  LinkNavigation,
+  LogoNotFound,
+} from "./styled";
+import logoNotFound from "../../images/grim-reaper.svg";
+
+export function NotFound() {
+  return (
+    <WrapperNotFound>
+      <LogoNotFound src={logoNotFound} />
+      <BoxDescriptionNotFound>
+        <TitleNotFound>Strange, but there is no such page</TitleNotFound>
+        <SubtitleNotFound>Why?</SubtitleNotFound>
+        <DescriptionNotFound>
+          The link may be out of date or have a typo. When the error occurs on
+          your browser side, Its code always starts with 4.
+        </DescriptionNotFound>
+        <SubtitleNotFound>What to do?</SubtitleNotFound>
+        <DescriptionNotFound>
+          Go to our <LinkNavigation to="/">home page</LinkNavigation>, which
+          definitely exists, or{" "}
+          <LinkNavigation to="/">write to us</LinkNavigation> - we wil fix
+          everything.
+        </DescriptionNotFound>
+      </BoxDescriptionNotFound>
+    </WrapperNotFound>
+  );
+}
