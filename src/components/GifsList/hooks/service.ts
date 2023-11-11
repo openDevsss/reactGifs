837054a269a0bf1490a9c18a8698eb1b930b7503
kirill <marchenko@finleo.ru>
2023-11-12 01:56:40 +0400

    fix: delete comments

--- src/components/GifsList/hooks/service.ts
+++ src/components/GifsList/hooks/service.ts
@@ -2,8 +2,4 @@ import axios from "axios";
 import { baseUrl } from "../../../constant";
 import { Gif } from "../../../types/GifType";
 
-// type GetGifsRequest = {
-//   data: Gif[];
-// };
-
 export const getGifs = () => axios.get<Gif[]>(`${baseUrl}/gifs`);
