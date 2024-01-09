5c621bb48e621c625d0b1fa74ca8229f92a05696
Andrey <andreydaskal2002@gmail.com>
2024-01-09 21:29:16 +0300

    added todo

--- src/components/EmptyGifs/EmptyGifs.tsx
+++ src/components/EmptyGifs/EmptyGifs.tsx
@@ -12,6 +12,7 @@ export default function EmptyGifs({ message, needLink }: EmptyGifsProps) {
   return (
     <EmptyGifsWrapper>
       <EmptyGifsMessage>{message}</EmptyGifsMessage>
+      {/*TODO:оставили url чтобы не было водных знаков */}
       <EmptyGifsImage
         src="https://img.freepik.com/free-vector/hand-drawn-no-data-concept_52683-127823.jpg?w=1380&t=st=1704754828~exp=1704755428~hmac=ae203df68952c791b2c38b5c9984515791795dfa5dd83c9a621b421d4cbeefae"
         alt=""
